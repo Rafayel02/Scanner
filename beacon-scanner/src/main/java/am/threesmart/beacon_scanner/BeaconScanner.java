3a44838a72e0658d938abb53ec213358c57ff59a
Rafael Shakhnazarian <rshakhnazari@adobe.com>
2023-02-11 22:58:31 +0400

    first commit

---
+++ beacon-scanner/src/main/java/am/threesmart/beacon_scanner/BeaconScanner.java
@@ -0,0 +1,19 @@
+package am.threesmart.beacon_scanner;
+
+public class BeaconScanner {
+
+    private static String health = "STOPPED";
+
+    private BeaconScanner() {
+    }
+
+    public static void start() {
+        health = "RUNNING";
+        // TODO implement logic
+    }
+
+    public static String healthCheck() {
+        return health;
+    }
+
+}
